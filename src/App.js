457ad717ec4d75bbf27fca0f1b72f7eb9fea3bae
Chris Martin <chris@crunchycloud.io>
2019-11-14 11:53:13 +1000

    trezor connect test bed

--- src/App.js
+++ src/App.js
@@ -2,35 +2,47 @@ import React, { useState } from 'react';
 import './App.css';
 import TrezorConnect from "trezor-connect";
 
-TrezorConnect.manifest({
-  email: 'chris@crunchycloud.io',
-  appUrl: 'http://localhost:8080'
+// TrezorConnect.manifest({
+//   email: 'chris@crunchycloud.io',
+//   appUrl: 'http://localhost:8080'
+// })
+
+TrezorConnect.init({
+  // origin: 'https://localhost:8088',
+  // iframeSrc: `${ DEFAULT_DOMAIN }iframe.html`,
+  // popup: true,
+  // popupSrc: `${ DEFAULT_DOMAIN }popup.html`,
+  // webusbSrc: `${ DEFAULT_DOMAIN }webusb.html`,
+  connectSrc: "http://localhost:8088/",
+  lazyLoad: true,
+  manifest: {
+    email: 'chris@crunchycloud.io',
+    appUrl: 'http://localhost:3000'
+  }
 })
 
 function App() {
   // use a transfer transaction by default
   const [text, setText] = useState(`{
-    "transaction": {
-      "type": 16724,
-      "networkType": 144,
-      "version": 36865,
-      "maxFee": "20000",
-      "deadline": "113248176649",
-      "signature": "",
-      "recipientAddress": {
-        "address": "SAIKV5OOWCQ3EHIBMJH7HR2GGKPXUG2VT4OE3FU7",
-        "networkType": 144
-      },
-      "mosaics": [
-        {
-          "amount": "1000000000",
-          "id": "308F144790CD7BC4"
-        }
-      ],
-      "message": {
-        "type": 0,
-        "payload": "This is a message"
+    "type": 16724,
+    "networkType": 144,
+    "version": 36865,
+    "maxFee": "20000",
+    "deadline": "113248176649",
+    "signature": "",
+    "recipientAddress": {
+      "address": "SAIKV5OOWCQ3EHIBMJH7HR2GGKPXUG2VT4OE3FU7",
+      "networkType": 144
+    },
+    "mosaics": [
+      {
+        "amount": "1000000000",
+        "id": "308F144790CD7BC4"
       }
+    ],
+    "message": {
+      "type": 0,
+      "payload": "This is a message"
     }
   }`);
   return (
@@ -47,8 +59,12 @@ function App() {
         <button
           onClick={async () => {
             const tx = JSON.parse(text)
-            const signTxResult = await TrezorConnect.nem2SignTransaction(tx)
-            console.log(signTxResult)
+            const signTxResult = await TrezorConnect.nem2SignTransaction({
+              path: `m/44'/43'/0'`,
+              transaction: tx
+            });
+
+            console.log("RESULT", signTxResult);
           }}
         >
           sign tx
