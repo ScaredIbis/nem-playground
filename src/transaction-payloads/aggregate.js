04a00b7329125e72909f3a613752328db7e1ae96
Thomas White <im.toms.inbox@gmail.com>
2019-12-17 17:19:34 +1000

    Changed aggregate test payload to match Nem desktop wallet

--- src/transaction-payloads/aggregate.js
+++ src/transaction-payloads/aggregate.js
@@ -5,44 +5,48 @@ export default {
     "maxFee": "100",
     "deadline": "113728610090",
     "signature": "",
-    "innerTransactions": [
+    "transactions": [
         {
-            "type": 16724,
-            "network": 152,
-            "version": 38913,
-            "maxFee": "0",
-            "deadline": "113728610090",
-            "signature": "",
-            "signerPublicKey": "778D0B0CF67E361BE774AF22FA27B29C82FE6A9160537EB6A6D9C6ABE4796778",
-            "recipientAddress": {
-                "address": "TB6YHDGPYV3TGEFE7Z3BH2EXJZNVJJTH3EWL76PD",
-                "networkType": 152
-            },
-            "mosaics": [
-                {
-                    "amount": "100",
-                    "id": "9ADF3B117A3C10CA"
+            "transaction": {
+                "type": 16724,
+                "network": 152,
+                "version": 38913,
+                "maxFee": "0",
+                "deadline": "113728610090",
+                "signature": "",
+                "signerPublicKey": "778D0B0CF67E361BE774AF22FA27B29C82FE6A9160537EB6A6D9C6ABE4796778",
+                "recipientAddress": {
+                    "address": "TB6YHDGPYV3TGEFE7Z3BH2EXJZNVJJTH3EWL76PD",
+                    "networkType": 152
+                },
+                "mosaics": [
+                    {
+                        "amount": "100",
+                        "id": "9ADF3B117A3C10CA"
+                    }
+                ],
+                "message": {
+                    "type": 0,
+                    "payload": "send 100 cat.currency to distributor"
                 }
-            ],
-            "message": {
-                "type": 0,
-                "payload": "send 100 cat.currency to distributor"
             }
         },
         {
+            "transaction": {
         
-            "type": 16717,
-            "network": 152,
-            "version": 38913,
-            "maxFee": "100",
-            "deadline": "113728610090",
-            "signature": "",
-            "signerPublicKey": "596FEAB15D98BFD75F1743E9DC8A36474A3D0C06AE78ED134C231336C38A6297",
-            "nonce": 3095715558,
-            "id": "3F127E74B309F220",
-            "flags": 7,
-            "divisibility": 100,
-            "duration": "123"
+                "type": 16717,
+                "network": 152,
+                "version": 38913,
+                "maxFee": "100",
+                "deadline": "113728610090",
+                "signature": "",
+                "signerPublicKey": "596FEAB15D98BFD75F1743E9DC8A36474A3D0C06AE78ED134C231336C38A6297",
+                "nonce": 3095715558,
+                "id": "3F127E74B309F220",
+                "flags": 7,
+                "divisibility": 100,
+                "duration": "123"
+            }
         }
     ],
     "cosignatures": [
